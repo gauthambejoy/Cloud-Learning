076e76f987031d3edb536b6b9a2bcf281a3b8a38
gauthambejoy <gauthambejoyprof@gmail.com>
2026-06-06 11:14:58 +0000

    Updated WebSiteUptime monitoring tool with handling status codes

--- Python-Projects/WebsiteUptimeMonitor/WebsiteUptimeMonitor.py
+++ Python-Projects/WebsiteUptimeMonitor/WebsiteUptimeMonitor.py
@@ -1,6 +1,28 @@
 import requests
 
-url=input("Enter the URL:")
-response=requests.get(url)
-print(response)
-print(response.status_code)
+def webup(url):
+    try:
+        response=requests.get(url)
+        stat_code=response.status_code
+        if 100 <= stat_code <200:
+            print("Informational")
+        elif 200 <= stat_code <= 299:
+            print("Success")
+            print(response)
+        elif 300 <= stat_code <=399:
+            print("Redirection")
+            print(response)
+        elif 400 <= stat_code <=499:
+            print("Client Error")
+            print(response)
+        elif stat_code >= 500:
+            print("Server error")
+            print(response)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        
+
+if __name__=="__main__":
+    url=input("Enter the URL:")
+    webup(url)
+
