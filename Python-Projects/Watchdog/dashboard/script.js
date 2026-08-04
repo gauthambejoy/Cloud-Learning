34c5fc76c406d23a342b1c7e055593ba8f1d8f69
gauthambejoy <gauthambejoyprof@gmail.com>
2026-08-04 03:15:12 +0000

    watchdog javascript update

--- Python-Projects/Watchdog/dashboard/script.js
+++ Python-Projects/Watchdog/dashboard/script.js
@@ -1,9 +1,11 @@
-<html>
-    <head>
-        <title>WatchDog</title>
-    </head>
-    <body>
-        <h1> Mini Monitoring Stack</h1>
-        <p>Backend: Running</p>
-    </body>
-</html>
+console.log("Script loaded");
+fetch("http://100.94.107.43/api/status")
+    .then(response =>{
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
